6e8227c1b4650795d69efde1cacd2d63ec4798cf
Pablo Levin <pablevin@gmail.com>
2021-06-03 19:01:07 -0300

    navegacion proyectos

--- js/functions.js
+++ js/functions.js
@@ -1,40 +1,39 @@
-window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden"; // class "loader hidden"
-});
 //VALUE 1
 var $num1 = "1/3";
 var $photographer1 = "Laura Pérez";
-var $title1 = "November is magic"
-var $titleShadow1 = "November"
-var $subtitle1 = "Perfect solution"
+var $title1 = "November is magic";
+var $titleShadow1 = "November";
+var $subtitle1 = "Perfect solution";
 var $text1='Holly Golightly is a flighty Manhattan party girl, who expects "money for the powder room as well as for cab fare" for her companionship. She has even gotten a lucrative once weekly job to visit notorious convict Sally Tomato in Sing Sing, she needing to report back to Sally.';
-var $img1 = "url(img/13.webp)"
+var $img1 = "url(img/13.webp)";
+var $url1= "hero1.html";
 
 //VALUE 2
 var $num2 = "2/3";
 var $photographer2 = "Juan Gómez";
-var $title2 = "Lighting for life"
-var $titleShadow2 = "Lighting"
-var $subtitle2 = "A way of life"
+var $title2 = "Lighting for life";
+var $titleShadow2 = "Lighting";
+var $subtitle2 = "A way of life";
 var $text2="Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor expression unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence. Steepest speaking up attended it as.";
-var $img2 = "url(img/17.webp)"
+var $img2 = "url(img/17.webp)";
+var $url2= "hero2.html";
 
 //VALUE 3
 var $num3 = "3/3";
 var $photographer3 = "Rafel Santos";
-var $title3 = "Power any shot"
-var $titleShadow3 = "Power"
-var $subtitle3 = "Fast and slow"
-var $text3="Bringing unlocked me an striking ye perceive. Mr by wound hours oh happy. Me in resolution pianoforte continuing we. Most my no spot felt by no. He he in forfeited furniture sweetness he arranging. Me tedious so to behaved written account ferrars moments. Too objection for elsewhere her preferred allowance her.";
-var $img3 = "url(img/2.webp)"
+var $title3 = "Power any shot";
+var $titleShadow3 = "Power";
+var $subtitle3 = "Fast and slow";
+var $text3="Bringing unlocked me an striking ye perceive. Mr by wound hours oh happy. Me in resolution pianoforte continuing we. Most my no spot felt by no. He he in forfeited furniture sweetness he arranging. Me tedious so to behaved written account ferrars moments.";
+var $img3 = "url(img/2.webp)";
+var $url3= "hero3.html";
 
 var i=1;
 //VANILLA JAVASCRIPT
 document.addEventListener('DOMContentLoaded', function () {
     var heroBtn = document.querySelector('#heroBtn');
     heroBtn.addEventListener('click', function() {
-        i++;
+        i++; 
         
         var sliderNum = document.querySelector('#sliderNum');
         sliderNum.textContent = eval('$num'+i);
@@ -50,6 +49,9 @@ document.addEventListener('DOMContentLoaded', function () {
         sliderText.textContent = eval('$text'+i);
         var sliderImg = document.querySelector('#sliderImg');
         sliderImg.style.backgroundImage = eval('$img'+i);
+
+        var a = document.getElementById('readMore');
+        a.href = "hero"+i+".html";
 
         if (i == 1) {
             document.getElementById('slash1').className += ' current';
@@ -86,6 +88,8 @@ document.addEventListener('DOMContentLoaded', function () {
         sliderText.textContent = eval('$text'+i);
         var sliderImg = document.querySelector('#sliderImg');
         sliderImg.style.backgroundImage = eval('$img'+i);
+        var a = document.getElementById('readMore');
+        a.href = "hero"+i+".html";
         document.getElementById('slash1').className += ' current';
         document.getElementById('slash2').classList.remove("current");
         document.getElementById('slash3').classList.remove("current");
@@ -107,6 +111,8 @@ document.addEventListener('DOMContentLoaded', function () {
         sliderText.textContent = eval('$text'+i);
         var sliderImg = document.querySelector('#sliderImg');
         sliderImg.style.backgroundImage = eval('$img'+i);
+        var a = document.getElementById('readMore');
+        a.href = "hero"+i+".html";
         document.getElementById('slash2').className += ' current';
         document.getElementById('slash1').classList.remove("current");
         document.getElementById('slash3').classList.remove("current");
@@ -128,9 +134,16 @@ document.addEventListener('DOMContentLoaded', function () {
         sliderText.textContent = eval('$text'+i);
         var sliderImg = document.querySelector('#sliderImg');
         sliderImg.style.backgroundImage = eval('$img'+i);
+        var a = document.getElementById('readMore');
+        a.href = "hero"+i+".html";
         document.getElementById('slash3').className += ' current';
         document.getElementById('slash1').classList.remove("current");
         document.getElementById('slash2').classList.remove("current");
         i=0;
     });
 });
+
+// window.addEventListener("load", function() {
+//     var bannerNode = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
+//     bannerNode.parentNode.removeChild(bannerNode);
+// });
---
+++ js/loader.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+});
+
+
+// window.addEventListener("load", function() {
+//     var bannerNode = document.querySelector('[alt="www.000webhost.com"]').parentNode.parentNode;
+//     bannerNode.parentNode.removeChild(bannerNode);
+// });
