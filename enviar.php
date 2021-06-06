d7fd4ec890f3df6244d2e1e585171fcdf43d0080
Pablo Levin <pablevin@gmail.com>
2021-06-06 19:11:01 -0300

    mail

--- enviar.php
+++ enviar.php
@@ -4,7 +4,7 @@ $correo = $_POST["correo"];
 
 $contenido = "Email de suscriptor:" .$correo;
 
-mail($destino, "contacto", $contenido);
+mail($destino, $contenido);
 // header("location:gracias.html");
 
 ?>
